14fe993807b6dd42c02931fb4e3703be8c666d2e
Victor Ravaglia <victor.ravaglia@hotmail.com>
2019-05-25 11:16:55 -0300

    PERL - Inicio funcao aparencia

--- GerenciamentoDeDados.pm
+++ GerenciamentoDeDados.pm
@@ -6,114 +6,112 @@ use File::Copy qw(move);
 
 # lista de mensagens de erro
 my %mensagensErro = (
-    "arquivo_01"  => "Erro ao abrir o arquivo: ",
-    "ok" => "Tudo funcionou como esperado",
-    "nome_sem_jogo" => "Nao foi encontrada uma partida associada ao nome",
+    "arquivo_01"           => "Erro ao abrir o arquivo: ",
+    "ok"                   => "Tudo funcionou como esperado",
+    "nome_sem_jogo"        => "Nao foi encontrada uma partida associada ao nome",
     "dificuldade_sem_jogo" => "Nao foi encontrada uma partida com essa dificuldade"
 
 );
 
-
 my $debug = 0;
 
 #transforma um arquivo texto em uma matriz em que as linhas representam as linhas do arquivo e as colunas representam as palavras de uma linha
-sub matrizPalavras{
-    open(my $arquivo,  "<", shift)
-        or die ("\n" . $mensagensErro{"arquivo_01"}. "\n" . $!);
+sub matrizPalavras {
+    open(my $arquivo, "<", shift)
+        or die("\n" . $mensagensErro{"arquivo_01"} . "\n" . $!);
     my @todas;
     my $i = 0;
     while (my $linha = <$arquivo>) {
         chomp $linha;
-        if (substr($linha, 0, 1) ne "#" && $linha =~ m/(\|\d+\|\d+)$|(\|\d+)$/)
-        {
-          my @palavras = split /\|/, $linha;
-          my $j = 0;
-          for my $k (0 .. scalar @palavras - 1){
-            $todas[$i][$j] = $palavras[$k];
-            $j++;
-          }
-          $i++;
+        if (substr($linha, 0, 1) ne "#" && $linha =~ m/(\|\d+\|\d+)$|(\|\d+)$/) {
+            my @palavras = split /\|/, $linha;
+            my $j = 0;
+            for my $k (0 .. scalar @palavras - 1) {
+                $todas[$i][$j] = $palavras[$k];
+                $j++;
+            }
+            $i++;
         }
     }
     return @todas;
 }
 
-sub listaPontuacoesMaiores{
+sub listaPontuacoesMaiores {
     my @linhas = matrizPalavras(shift);
-    if (scalar @linhas == 0){
+    if (scalar @linhas == 0) {
         return 0;
     }
     my $limite = shift;
-    if($debug == 1){
+    if ($debug == 1) {
         print Dumper @linhas;
     }
     my @pontuacoes;
-    for my $i (0 .. scalar @linhas - 1){
-        if (not $linhas[$i][2]){
+    for my $i (0 .. scalar @linhas - 1) {
+        if (not $linhas[$i][2]) {
             my %pontuacao;
             $pontuacao{"nome"} = $linhas[$i][0];
             $pontuacao{"pontos"} = $linhas[$i][1];
             push @pontuacoes, \%pontuacao;
         }
     }
-    if($debug == 1) {
+    if ($debug == 1) {
         print Dumper @pontuacoes;
     }
 
-    my @ordPontuacoes = sort { $a->{"pontos"} <=> $b->{"pontos"} } @pontuacoes;
+    my @ordPontuacoes = sort {$a->{"pontos"} <=> $b->{"pontos"}} @pontuacoes;
 
-    if($debug == 1) {
+    if ($debug == 1) {
         print Dumper @ordPontuacoes;
     }
 
     my $n = scalar @ordPontuacoes - $limite;
-    if ($n < 0){
+    if ($n < 0) {
         $n = 0;
     }
     return splice @ordPontuacoes, $n;
 }
 
 
-sub salvaPerfil{
+sub salvaPerfil {
     my $arquivo = shift;
     my $nome = shift;
     my $pontuacao = shift;
     my $dificuldade = shift;
     my $status = 0;
-    open my $in,  '<',  $arquivo      or die ("\n" . $mensagensErro{"arquivo_01"}. "\n" . $!);
-    open my $out, '>', "$arquivo.new" or die ("\n" . $mensagensErro{"arquivo_01"}. "\n" . $!);
+    open my $in, '<', $arquivo or die("\n" . $mensagensErro{"arquivo_01"} . "\n" . $!);
+    open my $out, '>', "$arquivo.new" or die("\n" . $mensagensErro{"arquivo_01"} . "\n" . $!);
 
     my $novo = 1;
 
     while (my $linha = <$in>) {
         chomp $linha;
-        if($debug == 1) {
+        if ($debug == 1) {
             print $linha . "\n";
         }
-        if($linha =~ /$nome\|\d+\|\d+/){
-            if($debug == 1) {
+        if ($linha =~ /$nome\|\d+\|\d+/) {
+            if ($debug == 1) {
                 print "Perfil encontrado: " . $linha . "\n";
             }
             print $out $nome . "\|" . $pontuacao . "\|" . $dificuldade;
-            if($debug == 1) {
+            if ($debug == 1) {
                 print "Novo perfil: " . $linha . "\n";
             }
             $novo = 0;
         }
-        else{
+        else {
             print $out $linha;
         }
         print $out "\n";
     }
-    if($novo > 0){
-        if($debug == 1) {
+    if ($novo > 0) {
+        if ($debug == 1) {
             print "Criando novo perfil";
         }
         print $out $nome . "\|" . $pontuacao . "\|" . $dificuldade;
         $status = 1;
     }
     close $out;
-    move "$arquivo.new" , $arquivo;
+    move "$arquivo.new", $arquivo;
     return $status;
 }
 
@@ -124,33 +122,27 @@ sub salvaPerfil{
 #
 #  Retorna: Pontuacao atual da partida
 #           Codigo de erro
-sub leJogoEmAndamento
-{
-  my $nome = $_[0];
-  my $pontuacao;
-  my $dificuldade = $_[1];
-  my @linhas = matrizPalavras($_[2]);
-  my $chaveErro = "ok";
+sub leJogoEmAndamento {
+    my $nome = $_[0];
+    my $pontuacao;
+    my $dificuldade = $_[1];
+    my @linhas = matrizPalavras($_[2]);
+    my $chaveErro = "ok";
 
-  for my $i (0 .. scalar @linhas)
-  {
-    if (defined $linhas [$i][2] && $dificuldade == $linhas [$i][2])
-    {
-      if ($linhas [$i][0] eq $nome)
-      {
-          $pontuacao = $linhas [$i][1];
-      }
-      else
-      {
-        $chaveErro = "nome_sem_jogo";
-      }
+    for my $i (0 .. scalar @linhas) {
+        if (defined $linhas[$i][2] && $dificuldade == $linhas[$i][2]) {
+            if ($linhas[$i][0] eq $nome) {
+                $pontuacao = $linhas[$i][1];
+            }
+            else {
+                $chaveErro = "nome_sem_jogo";
+            }
+        }
+        else {
+            $chaveErro = "dificuldade_sem_jogo";
+        }
     }
-    else
-    {
-      $chaveErro = "dificuldade_sem_jogo";
-    }
-  }
-  return $pontuacao, $chaveErro;
+    return $pontuacao, $chaveErro;
 }
 
 # sub listaPontuacoesDeJogador (nome, nivelDeRestricao, matriz de palvras )
@@ -163,67 +155,129 @@ sub leJogoEmAndamento
 #
 #  Falta: ver se vamos listar as partidas nao terminadas (sim)
 #
-sub listaPontuacoesDeJogador
-{
-  my $nome = $_[0];
-  my @partidas;
-  my $restricao = $_[1];
-  my @linhas = matrizPalavras($_[2]);
-  my $chaveErro = "ok";
-  my $nomeIterador;
-  my $pontuacaoIterador;
-  my $i;
+sub listaPontuacoesDeJogador {
+    my $nome = $_[0];
+    my @partidas;
+    my $restricao = $_[1];
+    my @linhas = matrizPalavras($_[2]);
+    my $chaveErro = "ok";
+    my $nomeIterador;
+    my $pontuacaoIterador;
+    my $i;
 
-  if ($restricao == 1)
-  {
-    $nome =~ tr/A-Z/a-z/;
-    $nome =~ tr/@/a/;
-    $nome =~ tr/&/e/;
-    $nome =~ tr/3/e/;
-    $nome =~ tr/1/i/;
-    $nome =~ tr/0/o/;
-  }
-
-  if ($restricao == 2)
-  {
-    $nome =~ tr/A-Z/a-z/;
-  }
-
-  for $i (0 .. scalar @linhas)
-  {
-    $nomeIterador = $linhas [$i][0];
-    $pontuacaoIterador = $linhas [$i][1];
-    if (defined $nomeIterador && defined $pontuacaoIterador)
-    {
-      if ($restricao == 2)
-      {
-        $nomeIterador =~ tr/A-Z/a-z/;
-      }
-
-      if ($restricao == 1)
-      {
-        $nomeIterador =~ tr/A-Z/a-z/;
-        $nomeIterador =~ tr/@/a/;
-        $nomeIterador =~ tr/&/e/;
-        $nomeIterador =~ tr/3/e/;
-        $nomeIterador =~ tr/1/i/;
-        $nomeIterador =~ tr/0/o/;
-      }
-
-      if ($nomeIterador =~ m/$nome/)
-      {
-        push (@partidas, $pontuacaoIterador);
-      }
-    } #if (defined $nomeIterador)
-    else
-    {
+    if ($restricao == 1) {
+        $nome =~ tr/A-Z/a-z/;
+        $nome =~ tr/@/a/;
+        $nome =~ tr/&/e/;
+        $nome =~ tr/3/e/;
+        $nome =~ tr/1/i/;
+        $nome =~ tr/0/o/;
     }
 
-    if ( @partidas == 0)
-    {
-      $chaveErro = "partidas_nao_encontradas"
+    if ($restricao == 2) {
+        $nome =~ tr/A-Z/a-z/;
     }
-  }
-  return @partidas;
+
+    for $i (0 .. scalar @linhas) {
+        $nomeIterador = $linhas[$i][0];
+        $pontuacaoIterador = $linhas[$i][1];
+        if (defined $nomeIterador && defined $pontuacaoIterador) {
+            if ($restricao == 2) {
+                $nomeIterador =~ tr/A-Z/a-z/;
+            }
+
+            if ($restricao == 1) {
+                $nomeIterador =~ tr/A-Z/a-z/;
+                $nomeIterador =~ tr/@/a/;
+                $nomeIterador =~ tr/&/e/;
+                $nomeIterador =~ tr/3/e/;
+                $nomeIterador =~ tr/1/i/;
+                $nomeIterador =~ tr/0/o/;
+            }
+
+            if ($nomeIterador =~ m/$nome/) {
+                push(@partidas, $pontuacaoIterador);
+            }
+        } #if (defined $nomeIterador)
+        else {
+        }
+
+        if (@partidas == 0) {
+            $chaveErro = "partidas_nao_encontradas"
+        }
+    }
+    return @partidas;
+}
+#Le um arquivo e converte para um vetor de linhas (strings)
+sub arquivoToVetor{
+    open(my $arquivo, "<", shift)
+        or die("\n" . $mensagensErro{"arquivo_01"} . "\n" . $!);
+    my @vetor;
+    while (my $linha = <$arquivo>) {
+        chomp $linha;
+        push @vetor, $linha;
+    }
+    return @vetor;
+}
+
+#Simplesmente imprime um vetor linha por linha
+sub printVector{
+    my @vetor = @_;
+    for my $i(0 .. scalar @vetor - 1){
+        print $vetor[$i] . "\n";
+    }
+}
+
+#Converte uma aparencia de base para lvl 1
+sub converteLvl1{
+    my @aparencia = @_;
+    for my $i(0 .. scalar @aparencia - 1){
+        $aparencia[$i] =~ s/\|/\:/g;
+        $aparencia[$i] =~ s/\=/\+/g;
+        $aparencia[$i] =~ s/o/0/g;
+        my ($trailing) = $aparencia[$i] =~ /^\s*/g;
+        $aparencia[$i] =~ s/^\s*/\#/g;
+        $aparencia[$i] =~ s/\s/\*/g;
+        $aparencia[$i] =~ s/\#/$trailing/g;
+    }
+    return @aparencia;
+}
+
+sub converteLvl2{
+    my @aparencia = @_;
+    for my $i(0 .. scalar @aparencia - 1){
+        $aparencia[$i] =~ s/\|/\!/g;
+        $aparencia[$i] =~ s/\*/O/g;
+        $aparencia[$i] =~ s/\=/\*/g;
+        $aparencia[$i] =~ s/\_/=/g;
+        my ($trailing) = $aparencia[$i] =~ /^\s*/g;
+        $aparencia[$i] =~ s/^\s*/\%/g;
+        $aparencia[$i] =~ s/\s/\#/g;
+        $aparencia[$i] =~ s/\%/$trailing/g;
+
+    }
+    return @aparencia;
+}
+
+sub configuraAparencia{
+    my $arquivoP = shift;
+    my $arquivoO = shift;
+    my @pBase = arquivoToVetor($arquivoP);
+    my @oBase = arquivoToVetor($arquivoO);
+    printVector(@pBase);
+    my @p1 = converteLvl1(@pBase);
+    print "\n";
+    printVector(@p1);
+    my @p2 = converteLvl2(@pBase);
+    print "\n";
+    printVector(@p2);
+    print "\n";
+    printVector(@oBase);
+    my @o1 = converteLvl1(@oBase);
+    print "\n";
+    printVector(@o1);
+    my @o2 = converteLvl2(@oBase);
+    print "\n";
+    printVector(@o2);
 }
 1;
--- perlTest.pl
+++ perlTest.pl
@@ -1,7 +1,7 @@
 use strict;
 use warnings FATAL => 'all';
 use Data::Dumper;
-use English qw' -no_match_vars ';
+use English qw'-no_match_vars';
 use Scalar::Util qw(looks_like_number);
 use Term::ReadKey;
 
@@ -12,7 +12,7 @@ use GerenciamentoDeDados;
 #print Dumper @teste;
 #GerenciamentoDeDados::salvaPerfil("dadosTeste.txt","raposo","666","1");
 
-sub printMenu{
+sub printMenu {
     print "|================================|\n";
     print "|           Trabalho 2           |\n";
     print "|================================|\n";
@@ -21,18 +21,20 @@ sub printMenu{
     print "| 2 ------------- Salva progresso|\n";
     print "| 3 --------------- Le jogo salvo|\n";
     print "| 4 ---- Le pontuacoes de jogador|\n";
+    print "| 5 -- Decide as mudancas de tema|\n";
     print "|================================|\n";
 }
 
-sub clear{
-    if($OSNAME eq "MSWin32"){
+sub clear {
+    if ($OSNAME eq "MSWin32") {
         system("cls");
-    }else{
+    }
+    else {
         system("clear");
     }
 }
 
-sub continuar{
+sub continuar {
     print "\nAperte \{ENTER\} para voltar ao menu principal...\n";
     my $key = <STDIN>;
 }
@@ -41,39 +43,52 @@ my $opcao;
 my $rodando = 1;
 my $debug = 1;
 
-
-while($rodando > 0){
+while ($rodando > 0) {
     clear();
     printMenu();
     print "Digite um numero de 0 a 5:\n";
     $opcao = <STDIN>;
     chomp $opcao;
-    if(looks_like_number($opcao)){
+    if (looks_like_number($opcao)) {
         clear();
-        if($opcao == 0){
+        if ($opcao == 0) {
             print "Saindo...\n";
             $rodando = 0;
         }
-        elsif($opcao == 1){
-          # Lista N maiores pontuacoes
-            print "Digite o nome do arquivo de pontuacoes:\n";
-            my $arquivo = <STDIN>;
-            chomp $arquivo;
+        elsif ($opcao == 1) {
+            # Lista N maiores pontuacoes
+            my $arquivo;
+            if ($debug == 1) {
+                $arquivo = "dadosTeste.txt";
+            }
+            else {
+                print "Digite o nome do arquivo de pontuacoes:\n";
+                $arquivo = <STDIN>;
+                chomp $arquivo;
+            }
+
             print "Digite o numero maximo de pontuacoes:\n";
             my $n = <STDIN>;
             chomp $n;
             my @pontuacoes = GerenciamentoDeDados::listaPontuacoesMaiores($arquivo, $n);
             my $size = scalar @pontuacoes;
-            print "Pontuacoes encontradas (" . $size ."):\n";
-            for my $i (0 .. $size - 1){
-                print "Nome: ". $pontuacoes[$size - $i - 1]{"nome"} . ", pontos: " . $pontuacoes[$size - $i - 1]{"pontos"} . "\n";
+            print "Pontuacoes encontradas (" . $size . "):\n";
+            for my $i (0 .. $size - 1) {
+                print "Nome: " . $pontuacoes[$size - $i - 1]{"nome"} . ", pontos: " . $pontuacoes[$size - $i - 1]{"pontos"} . "\n";
             }
             continuar();
-        }elsif ($opcao == 2){
-          # Salva progresso
-            print "Digite o nome do arquivo de pontuacoes:\n";
-            my $arquivo = <STDIN>;
-            chomp $arquivo;
+        }
+        elsif ($opcao == 2) {
+            # Salva progresso
+            my $arquivo;
+            if ($debug == 1) {
+                $arquivo = "dadosTeste.txt";
+            }
+            else {
+                print "Digite o nome do arquivo de pontuacoes:\n";
+                $arquivo = <STDIN>;
+                chomp $arquivo;
+            }
             print "Digite o nome do perfil:\n";
             my $nome = <STDIN>;
             chomp $nome;
@@ -83,52 +98,88 @@ while($rodando > 0){
             print "Digite a dificuldade atual:\n";
             my $dificuldade = <STDIN>;
             chomp $dificuldade;
-            my $status = GerenciamentoDeDados::salvaPerfil($arquivo,$nome,$pontos,$dificuldade);
-            if ($status == 0){
+            my $status = GerenciamentoDeDados::salvaPerfil($arquivo, $nome, $pontos, $dificuldade);
+            if ($status == 0) {
                 print "Perfil alterado com   sucesso.\n";
-            }elsif($status == 1){
+            }
+            elsif ($status == 1) {
                 print "Perfil criado com sucesso.\n";
             }
-            else{
+            else {
                 print "Ocorreu um erro de numero: " . $status;
             }
             continuar();
-        }elsif ($opcao == 3){
-        # Le jogo salvo
-          my $status;
-          my $pontuacao;
-          #print "Digite o nome do arquivo de pontuacoes:\n";
-          #my $arquivo = <STDIN>;
-          #chomp $arquivo;
-          my $arquivo = "dadosTeste.txt";
-          print "Digite o nome do perfil:\n";
-          my $nome = <STDIN>;
-          chomp $nome;
-          print "Digite a dificuldade atual:\n";
-          my $dificuldade = <STDIN>;
-          chomp $dificuldade;
-          ($pontuacao, $status) = GerenciamentoDeDados::leJogoEmAndamento($nome, $dificuldade, $arquivo);
-          print "Pontuacao: $pontuacao";
-          continuar();
-        }elsif ($opcao == 4){
-        # Le pontuacoes de jogador
-          my $status;
-          my @pontuacao;
-          #print "Digite o nome do arquivo de pontuacoes:\n";
-          #my $arquivo = <STDIN>;
-          #chomp $arquivo;
-          my $arquivo = "dadosTeste.txt";
-          print "Digite o nome do perfil:\n";
-          my $nome = <STDIN>;
-          chomp $nome;
-          print "Digite a restricao atual:\n";
-          my $restricao = <STDIN>;
-          chomp $restricao;
-          (@pontuacao, $status) = GerenciamentoDeDados::listaPontuacoesDeJogador($nome, $restricao, $arquivo);
-          print "Lista de pontos: @pontuacao";
-          continuar();
-        }elsif ($opcao == 5){
-
+        }
+        elsif ($opcao == 3) {
+            # Le jogo salvo
+            my $status;
+            my $pontuacao;
+            my $arquivo;
+            if ($debug == 1) {
+                $arquivo = "dadosTeste.txt";
+            }
+            else {
+                print "Digite o nome do arquivo de pontuacoes:\n";
+                $arquivo = <STDIN>;
+                chomp $arquivo;
+            }
+            print "Digite o nome do perfil:\n";
+            my $nome = <STDIN>;
+            chomp $nome;
+            print "Digite a dificuldade atual:\n";
+            my $dificuldade = <STDIN>;
+            chomp $dificuldade;
+            ($pontuacao, $status) = GerenciamentoDeDados::leJogoEmAndamento($nome, $dificuldade, $arquivo);
+            print "Pontuacao: $pontuacao";
+            continuar();
+        }
+        elsif ($opcao == 4) {
+            # Le pontuacoes de jogador
+            my $status;
+            my @pontuacao;
+            my $arquivo;
+            if ($debug == 1) {
+                $arquivo = "dadosTeste.txt";
+            }
+            else {
+                print "Digite o nome do arquivo de pontuacoes:\n";
+                $arquivo = <STDIN>;
+                chomp $arquivo;
+            }
+            print "Digite o nome do perfil:\n";
+            my $nome = <STDIN>;
+            chomp $nome;
+            print "Digite o codigo de restricao:\n";
+            print "1 -> Case insensitive e troca de simbolos por letras comuns\n";
+            print "2 -> Apenas case insensitive";
+            print "3 -> Nome exato";
+            my $restricao = <STDIN>;
+            chomp $restricao;
+            (@pontuacao, $status) = GerenciamentoDeDados::listaPontuacoesDeJogador($nome, $restricao, $arquivo);
+            print "Lista de pontos: @pontuacao";
+            continuar();
+        }
+        elsif ($opcao == 5) {
+            # Mudancas de tema
+            my $arquivoPlayer;
+            my $arquivoObstaculo;
+            print "Para um funcionamento justo do jogo os tamanhos maximos (em caracteres) dos arquivos de personagem e obstaculos sao 10x10.\n";
+            if ($debug == 1) {
+                $arquivoPlayer = "playerTeste.txt";
+                $arquivoObstaculo = "obstaculoTeste.txt";
+            }
+            else {
+                print "Digite o nome do arquivo de aparencia do jogador:\n";
+                $arquivoPlayer = <STDIN>;
+                chomp $arquivoPlayer;
+                print "Digite o nome do arquivo de aparencia de obstaculos:\n";
+                $arquivoObstaculo = <STDIN>;
+                chomp $arquivoObstaculo;
+            }
+            my @aparenciasJogador;
+            my @aparenciasObstaculos;
+            (@aparenciasJogador, @aparenciasObstaculos) = GerenciamentoDeDados::configuraAparencia($arquivoPlayer, $arquivoObstaculo);
+            continuar();
         }
     }
 
